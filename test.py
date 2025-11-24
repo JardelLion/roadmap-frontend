59a90204b7d93a2f2a8e7304d6aa754c6869b6b4
jardel.bernardo <jardel.bernardo@alien-group.com>
2025-11-25 00:28:01 +0100

    [New] Featue updates: superman addition, conflit fix, test - added 'iam a super man' feature - Fixed previous conflict in test.py - Added test updates

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-# test
+ # tes two  why
+# test forget
 iss  = 1
-=======
-iss  = 13
 # test this branch  i a new way
->>>>>>> f10564b ([NEW] add a file to cause conflit)
 print(f'this is  the issue {iss})
-
